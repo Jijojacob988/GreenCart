1469ec3e422883c6c471e7664c56e4dc1456908c
Jijo Jacob <jijojacob988@gmail.com>
2025-08-08 21:30:52 +0530

    feat(auth): added controller to verify authentication and return user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -75,3 +75,17 @@ export const login = async (req, res)=>{
         res.json({success: false, message: error.message});
     }
 }
+
+// check auth : /api/user/is-auth
+
+export const isAuth = async (req, res)=>{
+     try {
+        const { userId } = req.body;
+        const user = await User.findById(userId).select("-password")
+        return res.json({success: true, user})
+     } catch (error) {
+        console.log(error.message);
+        
+        res.json({success: false, message: error.message});
+     }
+}
