63a9265d6c6bb2ebbd50130b6638e76f17f30ac9
Jijo Jacob <jijojacob988@gmail.com>
2025-08-08 15:27:23 +0530

    feat(auth): added user login API and tested successfully with Postman

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
-import bcrypt from 'bcrypt.js'
+import bcrypt from 'bcryptjs';
+
 import jwt from "jsonwebtoken";
 
 
@@ -36,5 +37,41 @@ export const register = async (req, res)=>{
         
         res.json({success: false, message: error.message});
         
+    }
+}
+
+// Login user : /api/user/login
+
+export const login = async (req, res)=>{
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password)
+            return res.json({success: false, message: 'email and password are required'});
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.json({success: false, message: 'invalid email or password'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch)
+             return res.json({success: false, message: 'invalid email or password'});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+        })
+
+        return res.json({success: true, user: {email: user.email, name: user.name}})
+    } catch (error) {
+        console.log(error.message);
+        
+        res.json({success: false, message: error.message});
     }
 }
