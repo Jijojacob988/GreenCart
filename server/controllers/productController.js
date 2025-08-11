cf87e46064076dfbed9ecb65980e255d3b7f419b
Jijo Jacob <jijojacob988@gmail.com>
2025-08-11 12:16:15 +0530

    created an addcontroller function,using that we can add a new product in database

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,32 @@
-
+import { v2 as cloudinary } from "cloudinary"
+import Product from "../models/Product.js"
 
 // Add Product : /api/product/add
 export const addProduct = async (req, res)=>{
+    try {
+        let productData = JSON.parse(req.body.productData)
+
+        const images = req.files
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item)=>{
+               let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'});
+               return result.secure_url
+        
+        
+        })
+    )
+
+      await Product.create({...productData, image: imagesUrl})
+
+      res.JSON({success: true, message: "Product Added"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.JSON({ success: false, message: error.message })
+        
+        
+    }
 
 }
 // Get Product : /api/product/list
