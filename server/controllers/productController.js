d861297d091538eca89521450165fbb38f20553b
Jijo Jacob <jijojacob988@gmail.com>
2025-08-11 12:45:01 +0530

    created 3 more functions called get product,get a single product and change product in stock

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -31,13 +31,39 @@ export const addProduct = async (req, res)=>{
 }
 // Get Product : /api/product/list
 export const productList = async (req, res)=>{
+    try {
+        const products = await Product.find({})
+        res.json({success: true, products})
+    } catch (error) {
+        console.log(error.message);
+        res.JSON({ success: false, message: error.message })
+    }
 
 }
 // Get single Product : /api/product/id
 export const productById = async (req, res)=>{
 
-}
+    try {
+        const { id } = req.body
+        const product = await Product.findById(id)
+        res.json({success: true, product})
+    } catch (error) {
+         console.log(error.message);
+        res.JSON({ success: false, message: error.message })
+    }
+    }
+
+
 // Change Product inStock : /api/product/stock
 export const changeStock = async (req, res)=>{
 
-}
+    try {
+        const { id, inStock } = req.body
+        await Product.findByIdAndUpdate(id, {inStock})
+        res.json({success: true, message: "Stock Updated"})
+    } catch (error) {
+         console.log(error.message);
+        res.JSON({ success: false, message: error.message })
+    }
+    }
+
