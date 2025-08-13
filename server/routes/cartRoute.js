84386cdbb4857a3c83f78b8668a303b6da164436
Jijo Jacob <jijojacob988@gmail.com>
2025-08-13 10:58:33 +0530

    created onsubmithandler function,using that user can login in the seller dashboard

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,13 +1,13 @@
-// cartRoute.js
+
 
 import express from "express";
 import authUser from "../middlewares/authUser.js";
 import { updateCart } from "../controllers/cartController.js";
 
-// Use express.Router(), not mongoose.Router()
+
 const cartRouter = express.Router();
 
-// Update cart route
+
 cartRouter.post('/update', authUser, updateCart);
 
 export default cartRouter;
