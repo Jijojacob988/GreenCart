eb18177701fff98afca4f9ba4d0e2032a8a8d0b7
jijo jacob <jijojacob988@gmail.com>
2025-08-16 13:39:15 +0530

    feat(payment): restore Stripe checkout functionality

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
+import stripe from "stripe"
 
 // Place Order COD : /api/order/cod
 export const placeOrderCOD = async (req, res) => {
@@ -33,6 +34,80 @@ export const placeOrderCOD = async (req, res) => {
     });
 
     return res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// Place Order Stripe : /api/order/stripe
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const userId = req.userId; 
+    const { items, address } = req.body;
+    const {origin} = req.headers;
+
+    if (!address || !items || items.length === 0) {
+      return res.json({ success: false, message: "Invalid data" });
+    }
+
+    let productData = [];
+
+    // Calculate Amount Using Items
+    let amount = 0;
+    for (const item of items) {
+      const product = await Product.findById(item.product);
+      productData.push({
+        name: product.name,
+        price: product.offerPrice,
+        quantity: item.quantity,
+      });
+      if (!product) {
+        return res.json({ success: false, message: "Product not found" });
+      }
+      amount += product.offerPrice * item.quantity;
+    }
+
+    // Add Tax Charge (2%)
+    amount += Math.floor(amount * 0.02);
+
+    const order = await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "Online",
+    });
+
+    // Stripe Gateway Initialize
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    // create line items for stripe
+    const line_items = productData.map((item)=>{
+        return {
+          price_data: {
+            currency: "usd",
+            product_data:{
+              name: item.name,
+            },
+            unit_amount: Math.floor(item.price + item.price * 0.02) * 100
+          },
+          quantity: item.quantity,
+        }
+    })
+
+    // create session
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${origin}/loader?next=my-orders`,
+      cancel_url: `${origin}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        userId,
+      }
+    })
+
+    return res.json({ success: true, url: session.url });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
